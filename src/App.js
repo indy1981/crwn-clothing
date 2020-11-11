95350b8074f65f360ba20e9daaa98297f77a0879
Adrian Sanchez <aosanchez@gmail.com>
2020-11-10 21:25:26 -0300

    homepage now in compoenents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import './App.css';
-import HomePage from './homepage.component';
 
+import HomePage from './pages/homepage.component';
+import './App.css'
 function App() {
   return (
     <HomePage />
